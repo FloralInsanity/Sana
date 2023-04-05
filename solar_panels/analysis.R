8f0b8b49dd820f70effa266d2bd5e9658358b084
San <105588919+FloralInsanity@users.noreply.github.com>
2023-04-05 14:14:24 +0100

    Update analysis.R

--- solar_panels/analysis.R
+++ solar_panels/analysis.R
@@ -1,29 +1,3 @@
-# Problem
-# Energy prices are skyrocketing due to supply shortages.
-# Therefore, we need to look at renewable sources to meet the demand.
-
-# Question
-# Are solar panels a worthwhile investment for UK citizens in the long-term?
-
-# Motivations
-# Solar energy is both renewable and clean.
-# It will benefit the environment to use solar energy as opposed to sources like fossil fuels.
-
-# Goals
-# To gain insight into how much energy can be created by solar panels and the effect of typical UK weather (e.g. high rainfall) on that value.
-# Make predictions about future energy yield.
-
-# Nature of Data Collected
-# Data format: Excel
-# Data type: Structured
-# Time frame: Over 480 days July 2013-November 2014
-# Interval: Every 10 mins, every minute in the summer of 2014.
-# Source: UK Power Networks
-
-# Cleaning Dataset
-# Removed rows with missing values
-# Removed columns unnecessary for analysis
-
 library(ggplot2)
 library(Hmisc)
 
@@ -37,9 +11,3 @@ ggplot(data = solar_data, mapping = aes(x = Site, y = Rain)) + stat_summary(fun.
 ggplot(data = solar_data, mapping = aes(x = Site, y = Humidity)) + stat_summary(fun.data = mean_sdl, geom = "bar") 
 
 ggplot(data = solar_data, mapping = aes(x = Site, y = SolarEnergy)) + stat_summary(fun.data = mean_sdl, geom = "bar") 
-
-# Conclusions
-# Energy yield was between 65 and 155 watts per half hour. 130-310 watts/hour
-# It beneficial to invest in solar panels regardless of location in UK
-# There are other variables to consider like: type of solar panel, quality of equipment. 
-# Between 250-300 watts per hour is average, consistent with the output of the YMCA solar panels
